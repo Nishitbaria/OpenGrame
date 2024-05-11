763bbf277784d6573404f3cc910edf1b801ee1eb
Ashish kumar <ashishchumber34@gmail.com>
2024-05-11 19:56:21 +0530

    chore: Refactor AllUsers page to improve search functionality

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -38,12 +38,10 @@ const AllUsers = () => {
   const [searchValue, setSearchValue] = useState<string>("");
   const { data: creators, isLoading, isError: isErrorCreators } = useGetUsers();
   const debouncedSearch = useDebounce(searchValue, 500)
-  console.log("the seach key value ", debouncedSearch);
-
   const { data: searchedUsers, isFetching: isSearchFetching, isError } = useSearchUsers(debouncedSearch);
-  console.log("the searched users query error ", isError);
-  
+  const myusers = creators?.documents.filter((user) => user.username.includes(searchValue))
   const shouldShowSearchResults = searchValue !== "";
+  
   if (isErrorCreators) {
     toast({ title: "Something went wrong." });
 
@@ -54,25 +52,25 @@ const AllUsers = () => {
     <div className="common-container">
       <div className="user-container">
         <div className="flex justify-around w-full items-center">
-        <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
-        <div className="flex w-full gap-1 px-4 rounded-lg bg-dark-4">
-          <img
-            src="/assets/icons/search.svg"
-            width={24}
-            height={24}
-            alt="search"
-          />
-          <Input
-            type="text"
-            placeholder="Search users"
-            className="explore-search"
-            value={searchValue}
-            onChange={(e) => {
-              const { value } = e.target;
-              setSearchValue(value);
-            }}
-          />
-        </div>
+          <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+          <div className="flex w-full gap-1 px-4 rounded-lg bg-dark-4">
+            <img
+              src="/assets/icons/search.svg"
+              width={24}
+              height={24}
+              alt="search"
+            />
+            <Input
+              type="text"
+              placeholder="Search users"
+              className="explore-search"
+              value={searchValue}
+              onChange={(e) => {
+                const { value } = e.target;
+                setSearchValue(value);
+              }}
+            />
+          </div>
         </div>
         {isLoading && !creators ? (
           <Loader />
@@ -86,11 +84,11 @@ const AllUsers = () => {
             </ul>
           ) : shouldShowSearchResults ? (
             <ul className="user-grid">
-              {creators?.documents.filter((user) => user.username.includes(searchValue)).map((creator) => (
-                <li key={creator?.$id} className="flex-1 min-w-[200px] w-full  ">
+              {myusers?.length !== 0 ? myusers?.map((creator) => (
+                <li key={creator.$id} className="flex-1 min-w-[200px] w-full  ">
                   <UserCard user={creator} />
                 </li>
-              ))}
+              )) : <p className="w-full mt-10 text-center text-light-4">No results found</p>}
             </ul>
           ) : (
             <ul className="user-grid">
