2448be5f2c1747974b83a7c92620c22ff41a9ab6
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-05-12 10:17:57 +0530

    logout Alerting fix

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { Link, useNavigate, NavLink, useLocation } from "react-router-dom";
 import { Button } from "../ui/button";
 import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
@@ -7,6 +7,7 @@ import { sidebarLinks } from "@/constants";
 import { INavLink } from "@/types";
 
 const LeftSidebar = () => {
+  const [logout,setLogout]=useState(false)
   const navigate = useNavigate();
   const { user } = useUserContext();
   const { mutate: signOut, isSuccess } = useSignOutAccount();
@@ -16,6 +17,7 @@ const LeftSidebar = () => {
   }, [isSuccess]);
 
   return (
+    <>
     <nav className="leftsidebar">
       <div className="flex flex-col gap-11">
         <Link to="/" className="flex gap-3 items-center">
@@ -72,7 +74,7 @@ const LeftSidebar = () => {
         <Button
           variant="ghost"
           className="shad-button_ghost"
-          onClick={() => signOut()}
+          onClick={() => setLogout(true)}
         >
           <img src="/assets/icons/logout.svg" alt="logout" />
           <p className="small-medium lg-base-medium"> Logout </p>
@@ -83,6 +85,22 @@ const LeftSidebar = () => {
         </Link>
       </div>
     </nav>
+    {logout?
+    <div style={{position:'fixed',height:250,width:350,backgroundColor:'rgb(9 9 10 /1)',top:0,right:0,left:0,bottom:0,margin:'auto',display:'flex',flexDirection:'column',justifyContent:'space-evenly',alignItems:'center',borderRadius:12
+    }}>
+    <div style={{display:'flex',justifyContent:'center',alignItems:'center',gap:'2rem'}}>
+    <img src="/assets/icons/logout.svg" alt="logout" style={{width:40,height:40}} />
+          <p style={{fontSize:'2rem',fontWeight:'700'}}> Logout </p>
+    </div>
+    <p style={{textAlign:'center',padding:10}}>If you wish to log out, please click the 'Logout' button to proceed or click 'Cancel' to remain logged in.</p>
+    <div style={{display:'flex',justifyContent:'center',alignItems:'center',gap:'1rem'}}>
+      <button style={{backgroundColor:'rgb(255 0 105)',padding:'0.3rem 2rem',borderRadius:12,fontWeight:500,cursor:'pointer'}} onClick={()=>signOut()}>Logout</button>
+      <button style={{backgroundColor:'rgb(255 255 255)',padding:'0.3rem 2rem',borderRadius:12,fontWeight:500,color:'black',cursor:'pointer'}} onClick={()=>setLogout(false)}>Cancel</button>
+    </div>
+    </div>
+    :<></>
+}
+    </>
   );
 };
 
