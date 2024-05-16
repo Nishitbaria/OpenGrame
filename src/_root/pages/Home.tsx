32df983ea0eb5f15f0febabfc037c7dac4f9259c
Rohitha Pudu <rohithapudu@gmail.com>
2024-05-16 15:10:38 +0530

    update  src/_root/pages/Home.tsx with MyPostHome

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -4,6 +4,8 @@ import Loader from "../../components/shared/Loader";
 import PostCard from "@/components/shared/PostCard";
 import { Models } from "appwrite";
 
+import MyPostHome from "./MyPostHome";
+
 const Home = () => {
   const {
     data: posts,
@@ -27,6 +29,9 @@ const Home = () => {
   return (
     <div className="flex flex-1 ">
       <div className="home-container">
+      <div>
+       <MyPostHome/>
+      </div>
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full"> Home Feed</h2>
           {isPostLoading ? (
