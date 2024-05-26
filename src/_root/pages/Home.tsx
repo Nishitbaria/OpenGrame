336359117e8821459a73aea4948c2c769828299e
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-05-26 10:53:59 +0530

    chore: Refactor Home and StoriesContainer components for improved readability and performance

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -13,18 +13,18 @@ const Home = () => {
     isError: isErrorPosts,
   } = useGetRecentPosts();
 
-  // if (isErrorPosts) {
-  //   return (
-  //     <div className="flex flex-1">
-  //       <div className="home-container">
-  //         <p className="body-medium text-light-1">Something bad happened</p>
-  //       </div>
-  //       <div className="home-creators">
-  //         <p className="body-medium text-light-1">Something bad happened</p>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (isErrorPosts) {
+    return (
+      <div className="flex flex-1">
+        <div className="home-container">
+          <p className="body-medium text-light-1">Something bad happened</p>
+        </div>
+        <div className="home-creators">
+          <p className="body-medium text-light-1">Something bad happened</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col flex-1 ">
--- src/components/shared/StoriesContainer.tsx
+++ src/components/shared/StoriesContainer.tsx
@@ -23,7 +23,7 @@ export default function StoriesContainer() {
 
   if (isError) {
     return (
-      <div className="w-full h-20 flex justify-center items-center">
+      <div className="flex items-center justify-center w-full h-20">
         <h1>Something Went Wrong !!</h1>
       </div>
     );
@@ -32,13 +32,13 @@ export default function StoriesContainer() {
   return (
     <>
       {isLoading ? (
-        <div className="w-full h-20 flex justify-center items-center">
+        <div className="flex items-center justify-center w-full h-20">
           <Loader />
         </div>
       ) : (
         <div className="w-[86vw] sm:w-[640px] md:w-[500px] lg:w-full flex justify-center items-center relative">
           <button
-            className="hidden md:block absolute -left-[3%] top-1/2 transform -translate-y-1/2 z-10 bg-gray-900 p-1 text-center rounded-full text-sm text-[#de4bff] w-8 h-8"
+            className="hidden md:block absolute -left-[3%] top-1/2 transform -translate-y-1/2 z-10 bg-[#1D1D22] p-1 text-center rounded-full text-sm text-primary-500 w-8 h-8"
             onClick={scrollLeft}
           >
             &lt;
@@ -46,7 +46,7 @@ export default function StoriesContainer() {
 
           <div
             ref={containerRef}
-            className="w-full my-1 flex justify-start items-start space-x-3 overflow-x-auto p-1 px-2 rounded drop-shadow-xl no-scrollbar"
+            className="flex items-start justify-start w-full p-1 px-2 my-1 space-x-3 overflow-x-auto rounded drop-shadow-xl no-scrollbar"
           >
             {data?.map(
               (user) =>
@@ -71,7 +71,7 @@ export default function StoriesContainer() {
             )}
           </div>
           <button
-            className="hidden md:block absolute -right-[3%] top-1/2 transform -translate-y-1/2 bg-gray-900 p-1 text-center rounded-full text-sm text-[#de4bff] w-8 h-8"
+            className="hidden md:block absolute -right-[3%] top-1/2 transform -translate-y-1/2 bg-[#1D1D22] p-1 text-center rounded-full text-sm text-primary-500 w-8 h-8"
             onClick={scrollRight}
           >
             &gt;
