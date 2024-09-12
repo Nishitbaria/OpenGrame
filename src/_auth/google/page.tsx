00a7c6f0ba32f1b86996864e14ad1b3744fef6bd
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-09-12 22:33:23 +0530

    fix: Depedancy

--- src/_auth/google/page.tsx
+++ src/_auth/google/page.tsx
@@ -6,23 +6,23 @@ import { OAuthProvider } from "appwrite";
 
 const GoogleLogin = () => {
     return (
-            // <h1 className="text-2xl font-bold"></h1>
-            <div className=" flex mt-5 justify-center items-center">
-                <button
-                    className="flex justify-center items-center gap-2 border-2 border-slate-600 px-5 py-2 w-full bg-white rounded-lg"
-                    onClick={() => {
-                        account.createOAuth2Session(
-                            OAuthProvider.Google,
-                            "http://localhost:5173/", // Success URL
-                            "http://localhost:3000" // Failure URL
+        // <h1 className="text-2xl font-bold"></h1>
+        <div className=" flex mt-5 justify-center items-center">
+            <button
+                className="flex justify-center items-center gap-2 border-2 border-slate-600 px-5 py-2 w-full bg-white rounded-lg"
+                onClick={() => {
+                    account.createOAuth2Session(
+                        OAuthProvider.Google,
+                        "http://localhost:5173/", // Success URL
+                        "http://localhost:3000" // Failure URL
 
-                        );
-                    }}
-                >
-                    <FcGoogle size={30} />
-                    <span className=" text-black">Login with Google</span>
-                </button>
-            </div>
+                    );
+                }}
+            >
+                <FcGoogle size={30} />
+                <span className=" text-black">Login with Google</span>
+            </button>
+        </div>
     );
 };
 
